56a4a759c5b220ab4253cc067e76f0fab78eac56
Harshil Mistry <harshilmistry@harshils-laptop.lan>
2023-05-27 19:10:15 -0700

    OIDC Fixes

--- main.tf
+++ main.tf
@@ -92,6 +92,8 @@ resource "aws_eks_cluster" "eks_cluster" {
 
 # Create IAM OpenID Connect (OIDC) provider
 resource "aws_iam_openid_connect_provider" "oidc_provider" {
-  url            = aws_eks_cluster.eks_cluster.identity.0.oidc.0.issuer
-  client_id_list = ["sts.amazonaws.com"]
+  url                = aws_eks_cluster.eks_cluster.identity.0.oidc.0.issuer
+  client_id_list     = ["sts.amazonaws.com"]
+  thumbprint_list    = aws_eks_cluster.eks_cluster.identity.0.oidc.0.thumbprint_list
 }
+
